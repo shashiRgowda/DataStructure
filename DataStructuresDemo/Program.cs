beeb52d182edcdeed16e4297fe121396dc756555
shashiRgowda <shashikumartr64@gmail.com>
2021-05-31 12:29:01 +0530

    Added:DataStructuresDemo

---
+++ DataStructuresDemo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DataStructuresDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedList list = new LinkedList();
+            list.Add(56);
+            list.Add(30);
+            list.Add(70);
+            list.InsertAtParticularPosition(2, 30);
+            list.RemoveFirstNode();
+            list.Display();
+            Console.ReadKey();
+        }
+    }
+}
