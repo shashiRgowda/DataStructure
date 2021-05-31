677802775bca5334e203248a7140c524e9cd781f
shashiRgowda <shashikumartr64@gmail.com>
2021-05-31 12:39:22 +0530

    Added:StackUsingLinkedList

---
+++ StackUsingLinkedList/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StackUsingLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LinkedListStack linkedListStack = new LinkedListStack();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+            //linkedListStack.Peek(); top.data
+            //linkedListStack.Pop();
+            //linkedListStack.IsEmpty();
+            //linkedListStack.Display();
+            Console.ReadKey();
+        }
+    }
+}
